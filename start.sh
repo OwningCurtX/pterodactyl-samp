65822ea3ba4eda38c038fb9b4773e9e58e494549
Joseph <S2013190681@firstasia.edu.ph>
2017-10-10 20:15:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,6 +25,29 @@ fi
 
 if [ -f "/home/container/server.cfg" ]; then
     echo "server.cfg exists, not generating file."
+    echo "GameNode.Pro: Checking max player slot."
+    if [[ $(grep "maxplayers ${PLAYERSLOT}" /home/container/server.cfg) ]]; then 
+        echo "GameNode.Pro: Nothing wrong on max player slot. Continuing the process..."
+    else
+        echo "GameNode.Pro: There's wrong on max player slot. Generating another configuration file."
+        rm /home/container/server.cfg
+        echo "lanmode 0
+        rcon_password changeme
+        maxplayers ${PLAYERSLOT}
+        port ${SERVER_PORT}
+        hostname SA-MP 0.3 Server
+        gamemode0 grandlarc 1
+        filterscripts base gl_actions gl_property gl_realtime
+        announce 0
+        query 1
+        weburl www.sa-mp.com
+        maxnpc 0
+        onfoot_rate 40
+        incar_rate 40
+        weapon_rate 40
+        stream_distance 300.0
+        stream_rate 1000" > /home/container/server.cfg
+    fi
 else
     echo "lanmode 0
 rcon_password changeme
