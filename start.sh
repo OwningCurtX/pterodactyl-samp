58c8bef50cd6d0adf5023f50e44c919afe75e70b
Joseph <S2013190681@firstasia.edu.ph>
2017-10-10 20:45:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,13 +14,9 @@ else
     tar -zxf samp037svr_R2-1.tar.gz
 
     cp -r samp03/* /home/container/
-    echo "Copiado samp03 SI"
     rm -r /home/container/.tmp-build
-    echo "Removido .tmpbuild SI"
     cd /home/container
-    echo "cd home container SI"
     chmod 700 *
-    echo "CHMOD 700 TODO SI"
 fi
 
 if [ -f "/home/container/server.cfg" ]; then
@@ -29,27 +25,27 @@ if [ -f "/home/container/server.cfg" ]; then
     if [[ $(grep "maxplayers ${PLAYERSLOT}" /home/container/server.cfg) ]]; then 
         echo "GameNode.Pro: Nothing wrong on max player slot. Continuing the process..."
     else
-        echo "GameNode.Pro: There's wrong on max player slot. Generating another configuration file."
-        rm /home/container/server.cfg
-        echo "lanmode 0
-        rcon_password changeme
-        maxplayers ${PLAYERSLOT}
-        port ${SERVER_PORT}
-        hostname SA-MP 0.3 Server
-        gamemode0 grandlarc 1
-        filterscripts base gl_actions gl_property gl_realtime
-        announce 0
-        query 1
-        weburl www.sa-mp.com
-        maxnpc 0
-        onfoot_rate 40
-        incar_rate 40
-        weapon_rate 40
-        stream_distance 300.0
-        stream_rate 1000" > /home/container/server.cfg
+echo "GameNode.Pro: There's wrong on max player slot. Generating another configuration file."
+rm /home/container/server.cfg
+echo "lanmode 0
+rcon_password changeme
+maxplayers ${PLAYERSLOT}
+port ${SERVER_PORT}
+hostname SA-MP 0.3 Server
+gamemode0 grandlarc 1
+filterscripts base gl_actions gl_property gl_realtime
+announce 0
+query 1
+weburl www.sa-mp.com
+maxnpc 0
+onfoot_rate 40
+incar_rate 40
+weapon_rate 40
+stream_distance 300.0
+stream_rate 1000" > /home/container/server.cfg
     fi
 else
-    echo "lanmode 0
+echo "lanmode 0
 rcon_password changeme
 maxplayers 50
 port ${SERVER_PORT}
